4241d2f5fae60db65184ca6a97b0bce85fe21a77
Alexandra Swart <answart@sbcglobal.net>
2014-01-21 12:34:53 -0800

    Add a MainPages controller

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,13 @@
+class MainPagesController < ApplicationController
+  def home
+  end
+
+  def portfolio
+  end
+
+  def current
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ZurbTest::Application.routes.draw do
+  get "main_pages/home"
+  get "main_pages/portfolio"
+  get "main_pages/current"
+  get "main_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
