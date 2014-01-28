ba376cb886d8376ee9a112c61cc379a7be7a2e34
Alexandra Swart <answart@sbcglobal.net>
2014-01-28 15:52:42 -0800

    Foundation and Views for main_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 ZurbTest::Application.routes.draw do
-  get "main_pages/home"
-  get "main_pages/portfolio"
-  get "main_pages/current"
-  get "main_pages/contact"
+
+  root "main_pages#home"
+
+  get "main_pages/portfolio" => "main_pages#portfolio"
+  get "main_pages/current" => "main_pages#current"
+  get "main_pages/contact" => "main_pages#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
